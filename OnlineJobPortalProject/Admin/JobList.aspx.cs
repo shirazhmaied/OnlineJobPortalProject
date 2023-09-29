0346f0af92e2ef0f9091e6e655013efa9d5357f3
Shiraz Hmaied <shiraz.hmaied@st.ul.edu.lb>
2023-09-30 01:20:34 +0300

    sevendone

---
+++ OnlineJobPortalProject/Admin/JobList.aspx.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OnlineJobPortalProject.Admin
+{
+    public partial class JobList : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        DataTable dt;
+        string str = ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+
+        protected void Page_PreRender (object sender, EventArgs e)
+        {
+            if (Session["Admin"] == null)
+            {
+                Response.Redirect("../User/Login.aspx");
+            }
+            //check if request is not a postback, which means it's the initial page
+            //to ensure loading jobs on initial page 
+            if (!IsPostBack)
+            {
+                ShowJob();
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ShowJob();
+            
+        }
+        private void ShowJob()
+        {
+            string query = string.Empty;
+            con = new SqlConnection(str);
+            query = @"Select Row_Number() over(order by (Select 1)) as [Sr.No], JobId, Title, NoOfPost, Qualification, Experience, LastDateToApply, CompanyName, Country, State, CreateDate from Jobs";
+            //This SQL command selects a row number using the Row_Number() function that generates sequential row nbs
+            //and assigns it an alias[Sr.No], ordering the rows by a constant value(1).
+            //This effectively assigns a sequential number to each row in the result set.
+            cmd = new SqlCommand(query, con);
+            //we used disconnected mode because its easier to bind data in gridviw controls
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+
+            ShowJob();
+
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                GridViewRow row = GridView1.Rows[e.RowIndex];
+                int JobId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+                con = new SqlConnection(str);
+                cmd = new SqlCommand("Delete from jobs where JobId=@id", con);
+                cmd.Parameters.AddWithValue("@id", JobId);
+                con.Open();
+                int r = cmd.ExecuteNonQuery();
+                if (r > 0)
+                {
+                    lblMsg.Text = "Job deleted successfully";
+                    lblMsg.CssClass = "alert alert-success";
+
+                }
+                else
+                {
+                    lblMsg.Text = "sorry, couldn't delete job";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+                con.Close();
+                GridView1.EditIndex = -1;
+                ShowJob();
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if(e.CommandName == "EditJob")
+            {
+                Response.Redirect("NewJob.aspx?id=" +e.CommandArgument.ToString() );
+            }
+        }
+    }
+}
--- OnlineJobPortalProject/User/Login.aspx.designer.cs
+++ OnlineJobPortalProject/User/Login.aspx.designer.cs
@@ -11,52 +11,61 @@ namespace OnlineJobPortalProject.User
 {
 
 
-    public partial class UserMaster
+    public partial class Login
     {
 
         /// <summary>
-        /// head control.
+        /// LblMsg control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        protected global::System.Web.UI.WebControls.Label LblMsg;
 
         /// <summary>
-        /// form1 control.
+        /// txtUserName control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.TextBox txtUserName;
 
         /// <summary>
-        /// lbRegisterorProfile control.
+        /// txtPassword control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lbRegisterorProfile;
+        protected global::System.Web.UI.WebControls.TextBox txtPassword;
 
         /// <summary>
-        /// lbLoginorLogout control.
+        /// ddlLoginType control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton lbLoginorLogout;
+        protected global::System.Web.UI.WebControls.DropDownList ddlLoginType;
 
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// RequiredFieldValidator1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
+        /// btnLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogin;
     }
 }
